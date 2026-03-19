ee0c4fcd0d880bfbd6babf7b8acd1a9920fe42a9
smbvi <coddiingversee@gmail.com>
2026-03-19 12:47:39 +0530

    Initial solution for 001-TwoSum

---
+++ 001-TwoSum/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+         int x[]= new int[2];
+         for(int i=0; i<nums.length; i++){
+            for(int j=0; j<i; j++){
+                if(nums[i]+nums[j] == target){
+                    x[0]=i;
+                    x[1]=j;
+                }
+            }
+        }
+        return x;
+    }
+}
